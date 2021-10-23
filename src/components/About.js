743807f6c80b94d637045c8affb953a1b8376fc9
Luke G <lukegronert@gmail.com>
2021-10-23 16:10:05 +0800

    Edited styling in about.css to make sure layout is responsive

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ export default function About() {
                     <img src='./images/portfolio-pic.jpg' alt='Headshot' />
                 </article>
                 <article className="about-description">
-                    Yo
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vestibulum est a tellus mattis, sit amet tincidunt libero convallis. Sed vitae tellus quam. Curabitur posuere pharetra erat a condimentum. Curabitur ullamcorper quam quis felis lacinia, at feugiat ipsum hendrerit. Cras sagittis purus vel iaculis efficitur. Nullam in suscipit augue. Quisque dui sapien, ultrices nec elementum quis, fermentum nec sem. Nam aliquam tincidunt elit, venenatis pulvinar purus tempor sit amet. Mauris malesuada lacus et nisl volutpat cursus. Phasellus lacinia, mi id laoreet pretium, nisl nisi euismod orci, ac tincidunt elit nisi id ex. Vivamus volutpat molestie felis, in malesuada est consequat et. Vestibulum sed mattis risus, nec congue erat. Fusce vitae consectetur augue. Vivamus aliquam non magna at dictum. Nulla facilisi. Curabitur rhoncus mauris in accumsan blandit.
                 </article>
             </div>
         </section>
