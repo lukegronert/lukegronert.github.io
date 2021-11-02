80903a579e1bcfff2a6b66de0d6c261fd869ffe8
Luke G <lukegronert@gmail.com>
2021-11-02 14:17:55 +0800

    Installed react-animate-on-scroll and animate.css libraries

--- src/components/SkillItem.js
+++ src/components/SkillItem.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import '../styles/skillItem.css';
+import ScrollAnimation from 'react-animate-on-scroll';
 
 
 export default function SkillItem(props) {
     return (
-        <div className='skill-item'>
-            <img src={props.logo} className='skill-item-logo' />
-            <p className='skill-item-title'>{props.title}</p>
-        </div>
+        <ScrollAnimation animateIn='fadeIn'>
+            <div className='skill-item'>
+                <img src={props.logo} className='skill-item-logo' />
+                <p className='skill-item-title'>{props.title}</p>
+            </div>
+        </ScrollAnimation>
     )
 }
