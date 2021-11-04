9f296c0e31f507e8efbc44566131aa7087a4d825
lukegronert@gmail.com <lukegronert@gmail.com>
2021-11-04 04:03:50 -0700

    Adjusted animations for Banner and added a line under banner h2

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import '../styles/banner.css';
-import Flash from 'react-reveal/Flash';
+import Jump from 'react-reveal/Jump';
 import Pulse from 'react-reveal/Pulse';
 
 export default function Banner() {
     return (
         <section className='ui centered banner-section' id="home">
             <div className='banner-content'>
-                <Flash>
+                <Jump>
                     <h1>
                         Luke Gronert
                     </h1>
-                </Flash>
+                </Jump>
+                <hr />
                 <Pulse delay={1000} duration={2000} count={2}>
                 <p>
                     Front-End Web Developer
