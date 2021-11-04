2e0a51ff4cfe1b7e5d13d406181eeabf55dd08c0
lukegronert@gmail.com <lukegronert@gmail.com>
2021-11-04 02:58:15 -0700

    Added animation to ProjectCard using react-reveal

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,28 +1,34 @@
 import React from 'react';
 import '../styles/projectCard.css';
 import Logo from './Logo';
+import Fade from 'react-reveal/Fade';
+import Slide from 'react-reveal/Slide';
 
 export default function ProjectCard(props) {
     return (
-        <div className='project-card'>
-            <img src={props.image} className='project-image' />
-            <div className='project-content'>
-            <div className='project-header'>
-                <p className='project-title'>{props.title}</p>
-                <div className='project-technologies'>
-                    {props.technologies.map((technology) => {
-                        return (
-                            <Logo logo={technology.image} alt={technology.alt} key={technology.alt} />
-                        )
-                    })}
+        <Fade duration={2000}>
+            <Slide right duration={500}>
+                <div className='project-card'>
+                    <img src={props.image} className='project-image' />
+                    <div className='project-content'>
+                    <div className='project-header'>
+                        <p className='project-title'>{props.title}</p>
+                        <div className='project-technologies'>
+                            {props.technologies.map((technology) => {
+                                return (
+                                    <Logo logo={technology.image} alt={technology.alt} key={technology.alt} />
+                                )
+                            })}
+                        </div>
+                    </div>
+                        <p>{props.description}</p>
+                        <div className='project-links'>
+                            <a href={props.github} target='_blank' rel="noreferrer" className="ui button">View Github</a>
+                            <a href={props.website} target='_blank' rel="noreferrer" className="ui button">View Website</a>
+                        </div>
+                    </div>
                 </div>
-            </div>
-                <p>{props.description}</p>
-                <div className='project-links'>
-                    <a href={props.github} target='_blank' rel="noreferrer" className="ui button">View Github</a>
-                    <a href={props.website} target='_blank' rel="noreferrer" className="ui button">View Website</a>
-                </div>
-            </div>
-        </div>
+            </Slide>
+        </Fade>
     )
 }
--- src/components/SkillItem.js
+++ src/components/SkillItem.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import '../styles/skillItem.css';
-import Slide from 'react-reveal/Slide';
 import Fade from 'react-reveal/Fade';
 
 export default function SkillItem(props) {
     return (
-        <Fade>
-            <Slide bottom>
+        <Fade bottom duration={2000}>
                 <div className='skill-item'>
                     <img src={props.logo} className='skill-item-logo' />
                     <p className='skill-item-title'>{props.title}</p>
                 </div>
-            </Slide>
         </Fade>
     )
 }
