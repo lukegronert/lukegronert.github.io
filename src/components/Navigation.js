586ab866f955f1b611d8a132c26d1598a2bf5484
Luke G <lukegronert@gmail.com>
2021-11-19 00:04:43 +0800

    Added line next to nav links. Changed hover color to orange from coolors palette.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,10 +5,10 @@ export default function Navigation({contactClick}) {
     return (
         <nav className="nav">
             <ul className="nav-list">
-                <li><a href="#about">About</a></li>
-                <li><a href="#skills">Skills</a></li>
-                <li><a href="#projects">Projects</a></li>
-                <li><a href="#" onClick={() => contactClick()}>Contact</a></li>
+                |<li><a href="#about">About</a></li>
+                |<li><a href="#skills">Skills</a></li>
+                |<li><a href="#projects">Projects</a></li>
+                |<li><a href="#" onClick={() => contactClick()}>Contact</a></li>
             </ul>
         </nav>
     )
