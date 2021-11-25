c7f26af65d1df2a970201f7e4bd657c0e62dd62d
Luke G <lukegronert@gmail.com>
2021-11-25 18:04:10 +0800

    Edited descriptions of projects.

--- src/data/projects.js
+++ src/data/projects.js
@@ -17,7 +17,7 @@ import weatherApp from '../images/weatherApp.png';
 export const projectList = [
     {
         title: 'Climb Taiwan',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vestibulum est a tellus mattis, sit amet tincidunt libero convallis. Sed vitae tellus quam. Curabitur posuere pharetra erat a condimentum.',
+        description: 'A website that provides information about various rock climbing areas and their respective routes around Taiwan. Created as a freelance project with vanilla Javascript and the Bootstrap framework for quick and easy design.',
         image: climbTaiwan,
         github: 'https://github.com/ClimbTaiwan/ClimbTaiwanWebsite',
         technologies: [
@@ -42,7 +42,7 @@ export const projectList = [
     },
     {
         title: 'Portfolio Website',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vestibulum est a tellus mattis, sit amet tincidunt libero convallis. Sed vitae tellus quam. Curabitur posuere pharetra erat a condimentum.',
+        description: 'My personal portfolio used to display my experience as a web developer. Created with a focus on mobile-first design.',
         image: portfolio,
         github: 'https://github.com/lukegronert/PortfolioWebsite',
         technologies: [
@@ -67,7 +67,7 @@ export const projectList = [
     },
     {
         title: 'Teacher Dashboard',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vestibulum est a tellus mattis, sit amet tincidunt libero convallis. Sed vitae tellus quam. Curabitur posuere pharetra erat a condimentum.',
+        description: 'An apllication developed to make my life as a teacher easier by giving me a simple way to keep track of "bonus points" in the classroom. Created as a project led approach to solidifying my understanding of React.',
         image: 'https://media.istockphoto.com/photos/innovation-and-new-ideas-lightbulb-concept-with-question-mark-picture-id1279690557?b=1&k=20&m=1279690557&s=170667a&w=0&h=BnH7EPIgwwpPyKMfeBJr87Ql900NFKjLYknrzvfi3Dw=',
         github: 'https://github.com/lukegronert/TeacherDashboard',
         technologies: [
@@ -92,7 +92,7 @@ export const projectList = [
     },
     {
         title: 'Weather App',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vestibulum est a tellus mattis, sit amet tincidunt libero convallis. Sed vitae tellus quam. Curabitur posuere pharetra erat a condimentum.',
+        description: 'An application that displays the current weather of cities across the world. Created to enhance understanding of DOM manipulation and the use of the fetch API. ',
         image: weatherApp,
         github: 'https://github.com/lukegronert/WeatherApp',
         technologies: [
