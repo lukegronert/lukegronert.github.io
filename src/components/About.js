efd8b374b9706c5719ec2b45b32ff99e1037e089
Luke G <lukegronert@gmail.com>
2021-11-25 18:05:42 +0800

    Edited styling of about section. Added max-height to image. Centered description and increased font size, while also making font strictly 20px.

--- src/components/About.js
+++ src/components/About.js
@@ -13,16 +13,15 @@ export default function About() {
             <div className="about-content">
                 <img src={PersonalPhoto} alt="Picture of a smiling man who looks like quite the expert developer" />
                 <article className="about-description">
-                STILL WORKIN ON IT
-                <br />
-                Hey, I'm Luke and I live in Taipei, Taiwan. I am a front-end developer from the San Francisco Bay Area who graduated from 
-                San Francisco State University and graduated with a degree in International Business. After university, 
-                I moved abroad to teach English in Seoul, South Korea and Taipei, Taiwan. Near the end of my stay in Seoul, my 
-                off-and-on hobby of web development turned into a dedication to learn and improve to transition into a new career.
-                <br />
-                <br />
-                In my free time, I'm most likely reading, playing volleyball, playing video games, watching sports, finding new cafes, or 
-                just chatting with friends. 
+                    I am an American Front-End Web Developer currently living in Taipei, Taiwan. I enjoy learning 
+                    new things and solving problems.
+                    <br />
+                    <br />
+                    My goal is to continuously improve my skills as a developer, while providing quality 
+                    <br />
+                    <br />
+                    In my free time, you can find me reading, playing volleyball, playing video games, watching sports, finding new cafes, or 
+                    just chatting with friends. 
                 </article>
             </div>
         </section>
