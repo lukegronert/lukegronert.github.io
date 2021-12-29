3c45407c4fd3ed1abce33a96e1096a645c8be1c8
Luke G <lukegronert@gmail.com>
2021-12-29 12:04:19 +0800

    Rewrote about description. Increased font-size to 18 on desktop screens over 900px wide.

--- src/components/About.js
+++ src/components/About.js
@@ -18,23 +18,13 @@ export default function About() {
                 <img className='personal-photo' src={PersonalPhoto} alt="Picture of a smiling man who looks like quite the expert developer" />
                 <article className="about-description">
                     <p>
-                        Hi, I'm Luke! I'm a Front-End Web Developer from California currently living in Taipei, Taiwan.
-                        <br />
-                        <br />
-                        When it comes to coding, I enjoy:
-                        <ul>
-                            <li>learning new concepts, like Redux and Node.js</li>
-                            <li>helping people find solutions</li>
-                            <li>providing value in the workplace</li>
-                        </ul>
-                        On my days off, you can find me:
-                        <ul>
-                            <li>reading (at the moment, American Gods)</li>
-                            <li>playing volleyball as a middle blocker</li>
-                            <li>hanging out at home playing video games, like Subnautica and Wild Rift</li>
-                            <li>watching sports</li>
-                            <li>finding new cafes</li>
-                        </ul>
+                        Hi, I'm Luke &#8212; a front-end web developer from California currently living in Taipei, Taiwan.
+                        <br /><br />
+                        I enjoy constantly learning and coming up with creative and logical solutions through coding.
+                        <br /> <br />
+                        In my free time, you can find me competing on the volleyball court, reading Sci-fi/Fantasy novels, 
+                        and playing video games (like Subnautica and Wild Rift). 
+                        <br /><br />
                         Want to get in touch? <img src={downArrow} className='small-icon' alt='Arrow pointing down' />
                     </p>
                     <div>
