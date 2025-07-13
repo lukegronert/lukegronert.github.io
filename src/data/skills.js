3457f6fc9611efa940c34c9bc8c64afb0ea4aac9
Luke Gronert <lukegronert@gmail.com>
2025-07-13 14:09:23 +0800

    Added icons to skills section

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,109 +1,129 @@
-import html from '../images/html5.svg';
-import css from '../images/css.svg';
-import javascript from '../images/javascript.svg';
-import react from '../images/react.svg';
-import bootstrap from '../images/bootstrap.svg';
-import semanticui from '../images/semantic.svg';
-import npm from '../images/npm.svg';
-import redux from '../images/redux.svg';
-import nodejs from '../images/nodejs.svg';
-import sass from '../images/sass.svg';
-import gulp from '../images/gulp.svg';
-import github from '../images/github.svg';
-import git from '../images/git.svg';
-import webpack from '../images/webpack.svg';
-import typescript from '../images/typescript.svg';
-import mongodb from '../images/mongodb.svg';
-import api from '../images/api.svg';
-import materialui from '../images/material-ui.svg'
+import html from "../images/html5.svg";
+import css from "../images/css.svg";
+import javascript from "../images/javascript.svg";
+import react from "../images/react.svg";
+import bootstrap from "../images/bootstrap.svg";
+import semanticui from "../images/semantic.svg";
+import npm from "../images/npm.svg";
+import redux from "../images/redux.svg";
+import nodejs from "../images/nodejs.svg";
+import sass from "../images/sass.svg";
+import gulp from "../images/gulp.svg";
+import github from "../images/github.svg";
+import git from "../images/git.svg";
+import webpack from "../images/webpack.svg";
+import typescript from "../images/typescript.svg";
+import mongodb from "../images/mongodb.svg";
+import api from "../images/api.svg";
+import materialui from "../images/material-ui.svg";
+import tailwind from "../images/tailwind.svg";
+import firebase from "../images/firebase.svg";
+import nextjs from "../images/nextjs.svg";
+import cypress from "../images/cypress.svg";
+import jest from "../images/jest.svg";
 
 export const skillSet = [
-    {
-        level: 'proficient',
-        items: [
-            {
-                title: 'HTML',
-                logo: html
-            },
-            {
-                title: 'CSS',
-                logo: css
-            },
-            {
-                title: 'Javascript',
-                logo: javascript
-            },
-            {
-                title: 'React',
-                logo: react
-            },
-            {
-                title: 'Bootstrap',
-                logo: bootstrap
-            },
-            {
-                title: 'Semantic UI',
-                logo: semanticui
-            },
-            {
-                title: 'Github',
-                logo: github
-            },
-            {
-                title: 'API',
-                logo: api
-            },
-            {
-                title: 'Material UI',
-                logo: materialui
-            }
-        ]
-    },
-    {
-        level: 'familiar',
-        items: [
-            {
-                title: 'SASS',
-                logo: sass
-            },
-            {
-                title: 'npm',
-                logo: npm
-            },
-            {
-                title: 'Git',
-                logo: git
-            },
-            {
-                title: 'Webpack',
-                logo: webpack
-            }
-
-        ]
-    },
-    {
-        level: 'exploring',
-        items: [
-            {
-                title: 'Redux',
-                logo: redux
-            },
-            {
-                title: 'Node JS',
-                logo: nodejs
-            },
-            {
-                title: 'MongoDB',
-                logo: mongodb
-            },
-            {
-                title: 'Gulp',
-                logo: gulp
-            },
-            {
-                title: 'Typescript',
-                logo: typescript
-            }
-        ]
-    }
-]
+  {
+    level: "proficient",
+    items: [
+      {
+        title: "Javascript",
+        logo: javascript,
+      },
+      {
+        title: "React",
+        logo: react,
+      },
+      {
+        title: "TailwindCSS",
+        logo: tailwind,
+      },
+      {
+        title: "Next JS",
+        logo: nextjs,
+      },
+      {
+        title: "Bootstrap",
+        logo: bootstrap,
+      },
+      {
+        title: "Semantic UI",
+        logo: semanticui,
+      },
+      {
+        title: "Github",
+        logo: github,
+      },
+      {
+        title: "HTML",
+        logo: html,
+      },
+      {
+        title: "CSS",
+        logo: css,
+      },
+      {
+        title: "API",
+        logo: api,
+      },
+      {
+        title: "Material UI",
+        logo: materialui,
+      },
+    ],
+  },
+  {
+    level: "familiar",
+    items: [
+      {
+        title: "Firebase",
+        logo: firebase,
+      },
+      {
+        title: "SASS",
+        logo: sass,
+      },
+      {
+        title: "npm",
+        logo: npm,
+      },
+      {
+        title: "Git",
+        logo: git,
+      },
+      {
+        title: "Webpack",
+        logo: webpack,
+      },
+    ],
+  },
+  {
+    level: "exploring",
+    items: [
+      {
+        title: "Redux",
+        logo: redux,
+      },
+      {
+        title: "Node JS",
+        logo: nodejs,
+      },
+      {
+        title: "MongoDB",
+        logo: mongodb,
+      },
+      {
+        title: "Gulp",
+        logo: gulp,
+      },
+      {
+        title: "Cypress",
+        logo: cypress,
+      },
+      {
+        title: "Jest",
+        logo: jest,
+      },
+    ],
+  },
+];
